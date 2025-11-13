58a1e228500f7c7bd6bf30cee0d76e68d23fd1ef
vikas choudhary <vikaschoudhary@vikass-MacBook-Air.local>
2025-11-13 16:22:03 +0530

    upate in machinedetalis page

--- src/pages/MachineDetails.jsx
+++ src/pages/MachineDetails.jsx
@@ -64,7 +64,7 @@ const MachineDetails = ({ machine, goBack }) => {
   // const SHEET_Id = "15SBKzTJKzaqhjPI5yt5tKkrd3tzNuhm_Q9-iDO8n0B0";
 
 
-  const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
   console.log(API_BASE_URL)
 
 
